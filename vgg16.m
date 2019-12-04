c364173909522b6c7eed640039ac3c4afb2cd27d
Karolina <44366239+karolaak@users.noreply.github.com>
2019-12-04 11:18:18 +0100

    vgg16.m
    Result around 96%, quite sufficient with percent branch.

---
+++ vgg16.m
@@ -0,0 +1,86 @@
+digitDatasetPath = fullfile('C:\Users\Damian\Desktop\karola\proj\animals\');
+images = imageDatastore(digitDatasetPath, ...
+    'IncludeSubfolders',true,'LabelSource','foldernames');
+
+[trainingImages,validationImages] = splitEachLabel(images,0.7);
+
+net = vgg16;
+net.Layers(1)
+inputSize = net.Layers(1).InputSize;
+
+if isa(net,'SeriesNetwork') 
+  lgraph = layerGraph(net.Layers); 
+else
+  lgraph = layerGraph(net);
+end 
+
+[learnableLayer,classLayer] = findLayersToReplace(lgraph);
+[learnableLayer,classLayer] 
+
+numClasses = numel(categories(trainingImages.Labels));
+
+if isa(learnableLayer,'nnet.cnn.layer.FullyConnectedLayer')
+    newLearnableLayer = fullyConnectedLayer(numClasses, ...
+        'Name','new_fc', ...
+        'WeightLearnRateFactor',10, ...
+        'BiasLearnRateFactor',10);
+    
+elseif isa(learnableLayer,'nnet.cnn.layer.Convolution2DLayer')
+    newLearnableLayer = convolution2dLayer(1,numClasses, ...
+        'Name','new_conv', ...
+        'WeightLearnRateFactor',10, ...
+        'BiasLearnRateFactor',10);
+end
+
+lgraph = replaceLayer(lgraph,learnableLayer.Name,newLearnableLayer);
+
+newClassLayer = classificationLayer('Name','new_classoutput');
+lgraph = replaceLayer(lgraph,classLayer.Name,newClassLayer);
+figure('Units','normalized','Position',[0.3 0.3 0.4 0.4]);
+plot(lgraph)
+ylim([0,10])
+
+layers = lgraph.Layers;
+connections = lgraph.Connections;
+
+layers(1:10) = freezeWeights(layers(1:10));
+lgraph = createLgraphUsingConnections(layers,connections);
+
+pixelRange = [-30 30];
+scaleRange = [0.9 1.1];
+imageAugmenter = imageDataAugmenter( ...
+    'RandXReflection',true, ...
+    'RandXTranslation',pixelRange, ...
+    'RandYTranslation',pixelRange, ...
+    'RandXScale',scaleRange, ...
+    'RandYScale',scaleRange);
+augimdsTrain = augmentedImageDatastore(inputSize(1:2),trainingImages, ...
+    'DataAugmentation',imageAugmenter);
+
+augimdsValidation = augmentedImageDatastore(inputSize(1:2),validationImages);
+miniBatchSize = 10;
+valFrequency = floor(numel(augimdsTrain.Files)/miniBatchSize);
+options = trainingOptions('sgdm', ...
+    'MiniBatchSize',miniBatchSize, ...
+    'MaxEpochs',6, ...
+    'InitialLearnRate',3e-4, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',augimdsValidation, ...
+    'ValidationFrequency',valFrequency, ...
+    'Verbose',false, ...
+    'Plots','training-progress');
+
+net = trainNetwork(augimdsTrain,lgraph,options);
+
+[predictedLabels,probs] = classify(net,augimdsValidation);
+accuracy = mean(predictedLabels == validationImages.Labels)
+
+idx = randperm(numel(validationImages.Files),4);
+figure
+for i = 1:4
+    subplot(2,2,i)
+    I = readimage(validationImages,idx(i));
+    imshow(I)
+    label = predictedLabels(idx(i));
+    title(string(label) + ", " + num2str(100*max(probs(idx(i),:)),3) + "%");
+end
